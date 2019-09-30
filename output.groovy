d981cfe74e5842b78fab6e143169f4dfc1767dc7
Information-Change J-C <infchg.appspot@gmail.com>
2019-10-01 00:20:46 +0200

    also via java groovy
    also via java groovy

---
+++ output.groovy
@@ -0,0 +1,15 @@
+def h=9
+// log.info( "starting with  h=" + h)
+println "with h=${h}"
+
+// properties later println " ${DB_Server}"
+//Properties p = appContext.getBean('tcProperties')
+
+class ob {
+    def mt() {
+        println "ob.mt "
+    }
+}
+oo = new ob()
+oo.mt()
+
